d95023ed3e1ea409f147123cfaaa4655c6f64100
a33372 <hai.nguyen592000@gmail.com>
2022-04-28 09:56:58 +0700

    fix table orders db

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::resource('products', ProductController::class);
+Route::resource('product', ProductController::class);
 Route::resource('cart', CartController::class);
 Route::resource('checkout',CheckoutController::class);
 
