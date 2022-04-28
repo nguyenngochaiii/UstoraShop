e14035b1ae4085b06afeffb27efdccbbf7919ff8
a33372 <hai.nguyen592000@gmail.com>
2022-04-28 12:54:10 +0700

    fix conflicts

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -47,6 +47,7 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    
     public function show($id)
     {   
         $product = Product::findOrFail($id);
